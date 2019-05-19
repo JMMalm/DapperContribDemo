6d2f65e58f4f5d5cb786131b40b312347e48335b
Jordon Malm <jordon.malm@gmail.com>
2019-05-18 19:44:58 -0500

    Add the MVC project.

---
+++ DapperContribDemo.MVC/Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace DapperContribDemo.MVC.Controllers
+{
+	public class UsersController : Controller
+	{
+		public IActionResult Index()
+		{
+			return View();
+		}
+
+		public IActionResult Welcome(string name)
+		{
+			if (string.IsNullOrWhiteSpace(name))
+			{
+				ViewData["Message"] = "Welcome from the Users controller!";
+			}
+			else
+			{
+				ViewData["Message"] = HtmlEncoder.Default.Encode($"Hello {name}!");
+			}
+
+			return View();
+		}
+	}
+}
